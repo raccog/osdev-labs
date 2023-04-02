64980bb99517aa8b0fef355cd7822c2c82541007
Ryan Cohen <rcohenprogramming@gmail.com>
2023-04-02 16:26:48 -0400

    pl011_uart: Fix some bitfields to be `u8` instead of `u16`

--- libraries/developing-modules/src/aarch64/pl011_uart.rs
+++ libraries/developing-modules/src/aarch64/pl011_uart.rs
@@ -9,46 +9,86 @@ use tock_registers::{
 
 pub const UART0_BASE_ADDRESS: usize = 0x0900_0000;
 
+register_structs! {
+    pub Pl011Uart {
+        // Data Register, UARTDR
+        (0x000 => pub dr: ReadWrite<u16, Data::Register>),
+        (0x002 => _reserved0),
+        // Receive Status Register/Error Clear Register, UARTRSR/UARTECR
+        (0x004 => pub rsr_ecr: ReadWrite<u8, ReceiveStatus::Register>),
+        (0x005 => _reserved1),
+        // Flag Register, UARTFR
+        (0x018 => pub fr: ReadOnly<u16, Flag::Register>),
+        (0x01a => _reserved2),
+        // IrDA Low-Power Counter Register, UARTILPR
+        (0x020 => pub ilpr: ReadWrite<u8>),
+        (0x021 => _reserved3),
+        // Integer Baud Rate Register, UARTIBRD
+        (0x024 => pub ibrd: ReadWrite<u16>),
+        (0x026 => _reserved4),
+        // Fractional Baud Rate Register, UARTFBRD
+        (0x028 => pub fbrd: ReadWrite<u8, FractionalBaudRate::Register>),
+        (0x029 => _reserved5),
+        // Line Control Register, UARTLCR_H
+        (0x02c => pub lcr_h: ReadWrite<u8, LineControl::Register>),
+        (0x02d => _reserved6),
+        // Control Register, UARTCR
+        (0x030 => pub cr: ReadWrite<u16, Control::Register>),
+        (0x032 => _reserved7),
+        // Interrupt FIFO Level Select Register, UARTIFLS
+        (0x034 => pub ifls: ReadWrite<u8, InterruptFifoLevelSelect::Register>),
+        (0x035 => _reserved8),
+        // Interrupt Mask Set/Clear Register, UARTIMSC
+        (0x038 => pub imsc: ReadWrite<u16, InterruptMask::Register>),
+        (0x03a => _reserved9),
+        // Raw Interrupt Status Register, UARTRIS
+        (0x03c => pub ris: ReadOnly<u16, RawInterruptStatus::Register>),
+        (0x03e => _reserved10),
+        // Masked Interrupt Status Register, UARTMIS
+        (0x040 => pub mis: ReadOnly<u16, MaskedInterruptStatus::Register>),
+        (0x042 => _reserved11),
+        // Interrupt Clear Register, UARTICR
+        (0x044 => pub icr: WriteOnly<u16, InterruptClear::Register>),
+        (0x046 => _reserved12),
+        // DMA Control Register, UARTDMACR
+        (0x048 => pub dmacr: ReadWrite<u8, DmaControl::Register>),
+        (0x049 => _reserved13),
+        // Peripheral Identification Registers, UARTPeriphID0
+        (0xfe0 => pub periph_id0: ReadOnly<u8, PeripheralId0::Register>),
+        (0xfe1 => _reserved14),
+        // Peripheral Identification Registers, UARTPeriphID1
+        (0xfe4 => pub periph_id1: ReadOnly<u8, PeripheralId1::Register>),
+        (0xfe5 => _reserved15),
+        // Peripheral Identification Registers, UARTPeriphID2
+        (0xfe8 => pub periph_id2: ReadOnly<u8, PeripheralId2::Register>),
+        (0xfe9 => _reserved16),
+        // Peripheral Identification Registers, UARTPeriphID3
+        (0xfec => pub periph_id3: ReadOnly<u8, PeripheralId3::Register>),
+        (0xfed => _reserved17),
+        // PrimeCell Identification Registers, UARTPCellID0
+        (0xff0 => pub pcell_id0: ReadOnly<u8, PCellId0::Register>),
+        (0xff1 => _reserved18),
+        // PrimeCell Identification Registers, UARTPCellID1
+        (0xff4 => pub pcell_id1: ReadOnly<u8, PCellId1::Register>),
+        (0xff5 => _reserved19),
+        // PrimeCell Identification Registers, UARTPCellID2
+        (0xff8 => pub pcell_id2: ReadOnly<u8, PCellId2::Register>),
+        (0xff9 => _reserved20),
+        // PrimeCell Identification Registers, UARTPCellID3
+        (0xffc => pub pcell_id3: ReadOnly<u8, PCellId3::Register>),
+        (0xffd => _reserved21),
+        (0x1000 => @END),
+    }
+}
+
 register_bitfields! [
-    u16,
-    pub Data [
-        // Overrun Error
-        OE OFFSET(11) NUMBITS(1) [],
-        // Break Error
-        BE OFFSET(10) NUMBITS(1) [],
-        // Parity Error
-        PE OFFSET(9) NUMBITS(1) [],
-        // Framing Error
-        FE OFFSET(8) NUMBITS(1) [],
-        // Receive/transmit data
-        DATA OFFSET(0) NUMBITS(8) [],
-    ],
+    u8,
     pub ReceiveStatus [
-        // See `Data` above for descriptions of these fields
+        // See `Data` for descriptions of these fields
         OE 3,
         BE 2,
         PE 1,
         FE 0
-    ],
-    pub Flag [
-        // Ring indicator
-        RI 8,
-        // Transmit FIFO empty
-        TXFE 7,
-        // Receive FIFO full
-        RXFF 6,
-        // Transmit FIFO full
-        TXFF 5,
-        // Receive FIFO empty
-        RXFE 4,
-        // UART busy
-        BUSY 3,
-        // Data carrier detect
-        DCD 2,
-        // Data set ready
-        DSR 1,
-        // Clear to send
-        CTS 0
     ],
     pub FractionalBaudRate [
         // The fractional baud rate divisor
@@ -69,6 +109,121 @@ register_bitfields! [
         PEN OFFSET(1) NUMBITS(1) [],
         // Send break
         BRK OFFSET(0) NUMBITS(1) []
+    ],
+    pub InterruptFifoLevelSelect [
+        // Receive interrupt FIFO level select
+        RXIFLSEL OFFSET(3) NUMBITS(3) [
+            // Each of these correspond to how full a FIFO can get before an interrupt is triggered
+            ONE_EIGHTH = 0b000,
+            ONE_FOURTH = 0b001,
+            ONE_HALF = 0b010,
+            THREE_FOURTHS = 0b011,
+            SEVEN_EIGHTHS = 0b100
+        ],
+        // Transmit interrupt FIFO level select
+        TXIFLSEL OFFSET(0) NUMBITS(3) [
+            // Each of these correspond to how empty a FIFO can get before an interrupt is triggered
+            ONE_EIGHTH = 0b000,
+            ONE_FOURTH = 0b001,
+            ONE_HALF = 0b010,
+            THREE_FOURTHS = 0b011,
+            SEVEN_EIGHTHS = 0b100
+        ],
+    ],
+    pub DmaControl [
+        // DMA on error
+        DMAONERR 2,
+        // Transmit DMA enable
+        TXDMAE 1,
+        // Receive DMA enable
+        RXDMAE 0
+    ],
+    pub PeripheralId0 [
+        // First 8 bits of the part number
+        PartNumber0 OFFSET(0) NUMBITS(8) [
+            VALID_UART = 0x011
+        ],
+    ],
+    pub PeripheralId1 [
+        // First 4 bits of the designer ID
+        Designer0 OFFSET(4) NUMBITS(4) [],
+        // Last 4 bits of the part number
+        PartNumber1 OFFSET(0) NUMBITS(4) [
+            VALID_UART = 0x0
+        ],
+    ],
+    pub PeripheralId2 [
+        // Revision number
+        Revision OFFSET(4) NUMBITS(4) [
+            R1P0 = 0x0,
+            R1P1 = 0x1,
+            R1P3 = 0x2,
+            R1P5 = 0x3,
+        ],
+        // Last 4 bits of the designer ID
+        Designer1 OFFSET(0) NUMBITS(4) [],
+    ],
+    pub PeripheralId3 [
+        // Not sure what this configuration is used for
+        Configuration OFFSET(0) NUMBITS(8) [
+            VALID_UART = 0x00
+        ]
+    ],
+    pub PCellId0 [
+        UARTPCellID0 OFFSET(0) NUMBITS(8) [
+            VALID_PCELL = 0x0d
+        ]
+    ],
+    pub PCellId1 [
+        UARTPCellID1 OFFSET(0) NUMBITS(8) [
+            VALID_PCELL = 0xf0
+        ]
+    ],
+    pub PCellId2 [
+        UARTPCellID2 OFFSET(0) NUMBITS(8) [
+            VALID_PCELL = 0x05
+        ]
+    ],
+    pub PCellId3 [
+        UARTPCellID3 OFFSET(0) NUMBITS(8) [
+            VALID_PCELL = 0xb1
+        ]
+    ],
+];
+
+register_bitfields! [
+    u16,
+    pub Data [
+        // Overrun Error
+        OE OFFSET(11) NUMBITS(1) [],
+        // Break Error
+        BE OFFSET(10) NUMBITS(1) [],
+        // Parity Error
+        PE OFFSET(9) NUMBITS(1) [],
+        // Framing Error
+        FE OFFSET(8) NUMBITS(1) [],
+        // Receive/transmit data
+        DATA OFFSET(0) NUMBITS(8) [],
+    ],
+    pub Flag [
+        // Ring indicator
+        RI 8,
+        // Transmit FIFO empty
+        TXFE 7,
+        // Receive FIFO full
+        RXFF 6,
+        // Transmit FIFO full
+        TXFF 5,
+        // Receive FIFO empty
+        RXFE 4,
+        // UART busy
+        BUSY 3,
+        // Data carrier detect
+        DCD 2,
+        // Data set ready
+        DSR 1,
+        // Clear to send
+        CTS 0
     ],
     pub Control [
         // CTS hardware flow control enable
@@ -95,26 +250,6 @@ register_bitfields! [
         SIREN 1,
         // UART enable
         UARTEN 0
-    ],
-    pub InterruptFifoLevelSelect [
-        // Receive interrupt FIFO level select
-        RXIFLSEL OFFSET(3) NUMBITS(3) [
-            // Each of these correspond to how full a FIFO can get before an interrupt is triggered
-            ONE_EIGHTH = 0b000,
-            ONE_FOURTH = 0b001,
-            ONE_HALF = 0b010,
-            THREE_FOURTHS = 0b011,
-            SEVEN_EIGHTHS = 0b100
-        ],
-        // Transmit interrupt FIFO level select
-        TXIFLSEL OFFSET(0) NUMBITS(3) [
-            // Each of these correspond to how empty a FIFO can get before an interrupt is triggered
-            ONE_EIGHTH = 0b000,
-            ONE_FOURTH = 0b001,
-            ONE_HALF = 0b010,
-            THREE_FOURTHS = 0b011,
-            SEVEN_EIGHTHS = 0b100
-        ],
     ],
     pub InterruptMask [
         // Overrun error interrupt mask
@@ -212,138 +347,7 @@ register_bitfields! [
         // nUARTRI modem interrupt clear
         RIMIC 0
     ],
-    pub DmaControl [
-        // DMA on error
-        DMAONERR 2,
-        // Transmit DMA enable
-        TXDMAE 1,
-        // Receive DMA enable
-        RXDMAE 0
-    ],
-    pub PeripheralId0 [
-        // First 8 bits of the part number
-        PartNumber0 OFFSET(0) NUMBITS(8) [
-            VALID_UART = 0x011
-        ],
-    ],
-    pub PeripheralId1 [
-        // First 4 bits of the designer ID
-        Designer0 OFFSET(4) NUMBITS(4) [],
-        // Last 4 bits of the part number
-        PartNumber1 OFFSET(0) NUMBITS(4) [
-            VALID_UART = 0x0
-        ],
-    ],
-    pub PeripheralId2 [
-        // Revision number
-        Revision OFFSET(4) NUMBITS(4) [
-            R1P0 = 0x0,
-            R1P1 = 0x1,
-            R1P3 = 0x2,
-            R1P5 = 0x3,
-        ],
-        // Last 4 bits of the designer ID
-        Designer1 OFFSET(0) NUMBITS(4) [],
-    ],
-    pub PeripheralId3 [
-        // Not sure what this configuration is used for
-        Configuration OFFSET(0) NUMBITS(8) [
-            VALID_UART = 0x00
-        ]
-    ],
-    pub PCellId0 [
-        UARTPCellID0 OFFSET(0) NUMBITS(8) [
-            VALID_PCELL = 0x0d
-        ]
-    ],
-    pub PCellId1 [
-        UARTPCellID1 OFFSET(0) NUMBITS(8) [
-            VALID_PCELL = 0xf0
-        ]
-    ],
-    pub PCellId2 [
-        UARTPCellID2 OFFSET(0) NUMBITS(8) [
-            VALID_PCELL = 0x05
-        ]
-    ],
-    pub PCellId3 [
-        UARTPCellID3 OFFSET(0) NUMBITS(8) [
-            VALID_PCELL = 0xb1
-        ]
-    ],
 ];
-
-register_structs! {
-    pub Pl011Uart {
-        // Data Register, UARTDR
-        (0x000 => pub dr: ReadWrite<u16, Data::Register>),
-        (0x002 => _reserved0),
-        // Receive Status Register/Error Clear Register, UARTRSR/UARTECR
-        (0x004 => pub rsr_ecr: ReadWrite<u8, ReceiveStatus::Register>),
-        (0x005 => _reserved1),
-        // Flag Register, UARTFR
-        (0x018 => pub fr: ReadOnly<u16, Flag::Register>),
-        (0x01a => _reserved2),
-        // IrDA Low-Power Counter Register, UARTILPR
-        (0x020 => pub ilpr: ReadWrite<u8>),
-        (0x021 => _reserved3),
-        // Integer Baud Rate Register, UARTIBRD
-        (0x024 => pub ibrd: ReadWrite<u16>),
-        (0x026 => _reserved4),
-        // Fractional Baud Rate Register, UARTFBRD
-        (0x028 => pub fbrd: ReadWrite<u8, FractionalBaudRate::Register>),
-        (0x029 => _reserved5),
-        // Line Control Register, UARTLCR_H
-        (0x02c => pub lcr_h: ReadWrite<u8, LineControl::Register>),
-        (0x02d => _reserved6),
-        // Control Register, UARTCR
-        (0x030 => pub cr: ReadWrite<u16, Control::Register>),
-        (0x032 => _reserved7),
-        // Interrupt FIFO Level Select Register, UARTIFLS
-        (0x034 => pub ifls: ReadWrite<u8, InterruptFifoLevelSelect::Register>),
-        (0x035 => _reserved8),
-        // Interrupt Mask Set/Clear Register, UARTIMSC
-        (0x038 => pub imsc: ReadWrite<u16, InterruptMask::Register>),
-        (0x03a => _reserved9),
-        // Raw Interrupt Status Register, UARTRIS
-        (0x03c => pub ris: ReadOnly<u16, RawInterruptStatus::Register>),
-        (0x03e => _reserved10),
-        // Masked Interrupt Status Register, UARTMIS
-        (0x040 => pub mis: ReadOnly<u16, MaskedInterruptStatus::Register>),
-        (0x042 => _reserved11),
-        // Interrupt Clear Register, UARTICR
-        (0x044 => pub icr: WriteOnly<u16, InterruptClear::Register>),
-        (0x046 => _reserved12),
-        // DMA Control Register, UARTDMACR
-        (0x048 => pub dmacr: ReadWrite<u8, DmaControl::Register>),
-        (0x049 => _reserved13),
-        // Peripheral Identification Registers, UARTPeriphID0
-        (0xfe0 => pub periph_id0: ReadOnly<u8, PeripheralId0::Register>),
-        (0xfe1 => _reserved14),
-        // Peripheral Identification Registers, UARTPeriphID1
-        (0xfe4 => pub periph_id1: ReadOnly<u8, PeripheralId1::Register>),
-        (0xfe5 => _reserved15),
-        // Peripheral Identification Registers, UARTPeriphID2
-        (0xfe8 => pub periph_id2: ReadOnly<u8, PeripheralId2::Register>),
-        (0xfe9 => _reserved16),
-        // Peripheral Identification Registers, UARTPeriphID3
-        (0xfec => pub periph_id3: ReadOnly<u8, PeripheralId3::Register>),
-        (0xfed => _reserved17),
-        // PrimeCell Identification Registers, UARTPCellID0
-        (0xff0 => pub pcell_id0: ReadOnly<u8, PCellId0::Register>),
-        (0xff1 => _reserved18),
-        // PrimeCell Identification Registers, UARTPCellID1
-        (0xff4 => pub pcell_id1: ReadOnly<u8, PCellId1::Register>),
-        (0xff5 => _reserved19),
-        // PrimeCell Identification Registers, UARTPCellID2
-        (0xff8 => pub pcell_id2: ReadOnly<u8, PCellId2::Register>),
-        (0xff9 => _reserved20),
-        // PrimeCell Identification Registers, UARTPCellID3
-        (0xffc => pub pcell_id3: ReadOnly<u8, PCellId3::Register>),
-        (0xffd => _reserved21),
-        (0x1000 => @END),
-    }
-}
 
 impl Pl011Uart {
     pub unsafe fn new(base_addr: usize) -> &'static mut Self {
